312d5de85a114a2e00e9c6d72fd975d19b6b3bfe
Vinay <kondojuvinaykumar2004@gmail.com>
2025-12-22 09:09:28 +0530

    Updated Assignment 4

--- UstGlobal-Assignments/UST_Assignment_4/files_5.py
+++ UstGlobal-Assignments/UST_Assignment_4/files_5.py
@@ -2,9 +2,10 @@
 # specific word in a text file and display the line numbers where it appears.
 word= input("Enter the word to search: ")
 f= open("UstGlobal-Assignments/UST_Assignment_4/Data5.txt", "r")
+content=f.readlines()
 current_line = 1
 word_found = False
-for line in f:
+for line in content:
     if word.lower() in line.lower():
         print(f"Word found at line {current_line}")
         word_found = True
